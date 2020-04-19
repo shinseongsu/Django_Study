2ed2bfcbfa343a5ac0703e5e540162eb95c6b6e2
Shin seongsu <tlstjdtn321@naver.com>
2020-04-19 17:23:48 +0900

    django-admin 만들어보기

---
+++ chapter3/ReadMe.md
@@ -0,0 +1,15 @@
+# 1. 가상환경 생성하기
+python -m venv myvenv
+
+# 2. 가상환경에 접속하기
+source myvenv/bin/activate
+
+
+# 1. django install
+$ pip install django
+# 2. django version 확인
+$ python3 -m django --version
+# 2.*.* 출력된다면 성공!
+
+
+django-admin startproject mysite
---
+++ chapter3/mysite/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import get_object_or_404, render
+from polls.models import Question
+
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/index.html', {'question': question})
