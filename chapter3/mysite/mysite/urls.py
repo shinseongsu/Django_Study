cf045ebec32b57518205277a385d178bdd46326e
Shin seongsu <tlstjdtn321@naver.com>
2020-04-21 22:38:56 +0900

    / 화면 만들어보기

--- chapter3/mysite/mysite/urls.py
+++ chapter3/mysite/mysite/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 
 from django.contrib import admin
 from django.urls import path, include
+from mysite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home'),
     path('polls/', include('polls.urls')),
     path('books/', include('books.urls')),
 ]
