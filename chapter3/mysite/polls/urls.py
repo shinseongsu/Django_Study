fdee10d0e57ed813185206e02b08a76017153c06
Shin seongsu <tlstjdtn321@naver.com>
2020-04-22 23:55:30 +0900

    Django log 추가

--- chapter3/mysite/polls/urls.py
+++ chapter3/mysite/polls/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from . import views
+from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    # /polls/
+    path('', views.IndexView.as_view(), name='index'),
+
+    # /polls/99/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    # /polls/99/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    # /polls/99/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
